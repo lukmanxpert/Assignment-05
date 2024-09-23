d3092faf21222c2ce8c2a0c70064b3b55a80d42d
lukmanxpert <lukmanmiah2004@gmail.com>
2024-09-23 18:40:54 +0600

    added some functionalites

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('link is connected');
+// variables
+const donateBtn = document.getElementById('donate-btn')
+const historyBtn = document.getElementById('history-btn')
+const historySection = document.getElementById('history-section');
+const cardsSection = document.getElementById('cards-section')
+
+// donate and history button functions
+
+historyBtn.addEventListener('click', function(){
+    historySection.classList.remove('hidden');
+    cardsSection.classList.add('hidden');
+    historyBtn.classList.add('bg-primary');
+    donateBtn.classList.remove('bg-primary');
+})
+
+donateBtn.addEventListener('click', function(){
+    historySection.classList.add('hidden');
+    cardsSection.classList.remove('hidden');
+    donateBtn.classList.add('bg-primary');
+    historyBtn.classList.remove('bg-primary');
+})
+
+
